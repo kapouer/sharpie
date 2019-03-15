1af1e51e745ef8f4c4612f14b7227ea1b9ceae14
Jérémy Lal <kapouer@melix.org>
2019-03-15 10:07:30 +0100

    Test webp content negotiation

--- test/simple.js
+++ test/simple.js
@@ -72,6 +72,59 @@ describe("Sharpie middleware", function suite() {
 		});
 	});
 
+
+	it("should convert jpeg to webp", function() {
+		app.get('/images/*', function(req, res, next) {
+			if (req.query.raw === undefined) {
+				req.params.url = req.path + '?raw';
+				sharpie()(req, res, next);
+			} else {
+				req.url = req.path.substring('/images'.length);
+				next();
+			}
+		}, express.static(__dirname + '/images'));
+
+		return got('http://localhost:' + port + '/images/image.jpg', {
+			headers: {
+				Accept: 'image/webp,*/*'
+			},
+			encoding: null
+		}).then(function(res) {
+			should(res.statusCode).equal(200);
+			// should(res.body.length).lessThan(635);
+			should(res.headers['content-type']).equal('image/webp');
+			return sharp(res.body).metadata().then(function(meta) {
+				should(meta.format).equal('webp');
+			});
+		});
+	});
+
+	it("should not convert jpeg to webp", function() {
+		app.get('/images/*', function(req, res, next) {
+			if (req.query.raw === undefined) {
+				req.params.url = req.path + '?raw';
+				sharpie()(req, res, next);
+			} else {
+				req.url = req.path.substring('/images'.length);
+				next();
+			}
+		}, express.static(__dirname + '/images'));
+
+		return got('http://localhost:' + port + '/images/image.jpg', {
+			headers: {
+				Accept: '*/*'
+			},
+			encoding: null
+		}).then(function(res) {
+			should(res.statusCode).equal(200);
+			// should(res.body.length).lessThan(635);
+			should(res.headers['content-type']).equal('image/jpeg');
+			return sharp(res.body).metadata().then(function(meta) {
+				should(meta.format).equal('jpeg');
+			});
+		});
+	});
+
 	it("should resize a jpeg image and return 400 with bad params", function() {
 		app.get('/images/*', function(req, res, next) {
 			if (req.query.raw === undefined) {
